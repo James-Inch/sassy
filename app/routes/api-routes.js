683cf04db081003a209654352dccaefcca5a25e0
james inch <james.inch0103@gmail.com>
2019-01-20 18:41:28 -0500

    connected to database

---
+++ app/routes/api-routes.js
@@ -0,0 +1,14 @@
+// Dependencies - import connection from config/conn...
+var connection = require("../config/connection.js");
+
+//Routes 
+module.exports = function(app) {
+    app.get("/api/all", function(req,res) {
+        var dbQuery = "SELECT * FROM messages";
+
+        connection.query(dbQuery, function(err, result) {
+          if (err) throw err;
+          res.json(result);
+        });
+    })
+};
--- server.js
+++ server.js
@@ -9,9 +9,9 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "app/public"));
 
-
+require("./app/routes/api-routes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
