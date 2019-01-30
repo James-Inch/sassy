c3a4cfbd489acc6bf0ed67d362ae4344f2b810aa
james inch <james.inch0103@gmail.com>
2019-01-29 20:01:00 -0500

    abandon ship

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -1,3 +1,13 @@
 var path = require("path");
 
 //TODO: write html routes and require routes on server 
+module.exports = function(app) { 
+    // load Landing page 
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/views/index.html"));
+    });
+    // Load message page
+    app.get("/messages", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/views/messages.html"));
+    }); 
+};
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-// var path = require("path");
+var path=require("path");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,9 +11,19 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + "app/public"));
+app.use(express.static(path.join(__dirname,"public")));
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('css/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
 
 require("./app/routes/api-routes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
